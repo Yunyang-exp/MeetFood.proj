d3b224d96c79d8031be9fa0c20a20c1332ec3251
Yunyang <whitefeather4096@gmail.com>
2025-10-22 15:00:51 -0400

    js scripts added

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/production');
 
 exports.customerSignUp = async (req, res) => {
+    // data validation
     req.checkBody('email').exists().withMessage('Email is required');
     req.checkBody('password').exists().withMessage('Password is required');
     req.checkBody('confirmPassword').exists().withMessage('Password is required');
@@ -21,4 +22,134 @@ exports.customerSignUp = async (req, res) => {
       .checkBody('confirmPassword')
       .withMessage('Password confirmation does not match password')
       .custom((value, { req }) => value == req.body.password);
-    };
+    
+    // Pre-check
+    const errors = req.validationErrors();
+    if (errors || errors.length > 0) {
+        return res.status(400).json({ errors: errors});
+    }
+
+    //Parameter guard
+    const numOfParams = Object.keys(req.body).length;
+    if (numOfParams > 3) {
+        return res
+            .status(400)
+            .json({ errors: [{msg: 'Bad Request, too many parameters.'}]});
+    }
+    const {email, password} = req.body;
+
+    try {
+        // Check if the users already exist
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                errors: [{ msg: 'Email already registered, Please sign in.'}],
+            });
+        }
+
+        //Create a new user to save
+        user = new User({
+            email,
+            password,
+        });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        //save to the database
+        await user.save();
+
+        //prepare payload for jwt token
+        const payload = {
+            user: {
+                id: user.id,
+            },
+        };
+
+        //sign and return jwt token
+        jwt.sign(
+            payload,
+            config.jwtSecret,
+            { expiresIn: config.jwtExpireTime },
+            (err, token) => {
+                if (err) throw err;
+                res.status(200).json({
+                    message: 'User account created successfully.',
+                    token,
+                });
+            }
+        )
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
+
+
+exports.customerSignIn = async (req, res) => {
+    // data validation
+    req.checkBody('email').exists().withMessage('Email is required');
+    req.checkBody('password').exists().withMessage('Password is required');
+    req.checkBody('email').isEmail().withMessage('Please include a valid email');
+    
+    // Pre-check
+    const errors = req.validationErrors();
+    if (errors || errors.length > 0) {
+        return res.status(400).json({ errors: errors});
+    }
+
+    //Parameter guard
+    const numOfParams = Object.keys(req.body).length;
+    if (numOfParams > 2) {
+        return res
+            .status(400)
+            .json({ errors: [{msg: 'Bad Request, too many parameters.'}]});
+    }
+    const {email, password} = req.body;
+
+    try {
+        // Check if the users already exist
+        let user = await User.findOne({ email });
+
+        if (!user) {
+            return res
+            .status(400)
+            .json({
+                errors: [{ msg: 'No user registered by this email'}],
+            });
+        }
+
+        // if the user exists, compare if the req password is the same as the user encrypted password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res
+            .status(400)
+            .json({ errors: [{msg: 'Invalid Credentials.'}]});
+        }
+
+        //prepare payload for jwt token
+        const payload = {
+            user: {
+                id: user.id,
+            },
+        };
+
+        //sign and return jwt token
+        jwt.sign(
+            payload,
+            config.jwtSecret,
+            { expiresIn: config.jwtExpireTime },
+            (err, token) => {
+                if (err) throw err;
+                res.status(200).json({
+                    message: 'User account created successfully.',
+                    token,
+                });
+            }
+        )
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+};
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/user');
 
-
 // Customer sign up
 router.post('/signup', UserController.customerSignUp);
 
-// Customer sign in 
-router.post('/signin', UserController.customerSignIn)
+// Customer sign in
+router.post('/signin', UserController.customerSignIn);
